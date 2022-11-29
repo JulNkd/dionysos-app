3125fa646cccf8833c79badca0ff9e9eeece176b
Julien Nakad <julien.nakad@gmail.com>
2022-11-29 13:55:36 +0100

    change branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@ require "faker"
 
 Invitation.destroy_all
 User.destroy_all
-Event.destroy_all
-Budget.destroy_all
 Spending.destroy_all
+Budget.destroy_all
+Event.destroy_all
 
 user = User.new(first_name: "Joy",
                 last_name: "Klasen",
