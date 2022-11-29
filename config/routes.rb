fff76e10326fa168d96065998898ed68729d36c1
Adrien Mery Costa <psnw.adrien@gmail.com>
2022-11-29 17:47:47 +0100

    add photos(album) to show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :events, only: %i[show new create] do
+  resources :events, only: %i[show new create edit update] do
     resources :budgets, only: %i[new create show]
   end
   # Defines the root path route ("/")
