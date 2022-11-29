8c12443c90287f7c18865c5723fdc8e23744c647
Julien Nakad <julien.nakad@gmail.com>
2022-11-29 18:52:07 +0100

    conflicts resolved

--- app/controllers/spendings_controller.rb
+++ app/controllers/spendings_controller.rb
@@ -1,32 +1,17 @@
 class SpendingsController < ApplicationController
-<<<<<<< HEAD
   before_action :set_event, only: %i[new create]
-=======
-  before_action :set_event, only: %i[new create show]
->>>>>>> 3f58b39a1b4dca74cfa9e517a3cad5670ef00c3a
 
   def new
     @spending = Spending.new
   end
 
   def create
-<<<<<<< HEAD
   end
 
   def edit
   end
 
   def update
-=======
-    @spending = Spending.new(spending_params)
-    @spending.event = @event
-    #seulement si event.budget.budget_restant > spendings params
-    if @spending.save
-      redirect_to event_spendings_path, status: :see_other
-    else
-      render :new, status: :unprocessable_entity
-    end
->>>>>>> 3f58b39a1b4dca74cfa9e517a3cad5670ef00c3a
   end
 
   def index
@@ -38,11 +23,8 @@ class SpendingsController < ApplicationController
   def set_event
     @event = Event.find(params[:event_id])
   end
-<<<<<<< HEAD
-=======
 
   def spending_params
     params.require(:spending).permit(:amount, :category, :date)
   end
->>>>>>> 3f58b39a1b4dca74cfa9e517a3cad5670ef00c3a
 end
--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,8 +1,3 @@
 class Budget < ApplicationRecord
-  has_many :spendings
-<<<<<<< HEAD
-  has_one :budget through :spendings
-=======
   belongs_to :event
->>>>>>> 3f58b39a1b4dca74cfa9e517a3cad5670ef00c3a
 end
