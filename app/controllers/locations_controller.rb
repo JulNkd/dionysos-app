7bf3ffcb7df7b72210bc80d1c4edca6390975989
Joy Klasen <joy.klasen@gmail.com>
2022-11-29 12:39:54 +0100

    super

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,9 +4,21 @@ class LocationsController < ApplicationController
   end
 
   def new
-    
+    @location = Location.new
   end
 
   def create
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to locations_path(@locations)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name, :address, :capacity, :price, :availability, :visibility)
   end
 end
