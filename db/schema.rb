c31fe4078c19f82095228bff60cdb3c1347f98ac
Joy Klasen <joy.klasen@gmail.com>
2022-11-30 10:46:58 +0100

    album column in event deleted

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 3022_11_28_163829) do
-
+ActiveRecord::Schema[7.0].define(version: 3022_11_28_163830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,7 +57,6 @@ ActiveRecord::Schema[7.0].define(version: 3022_11_28_163829) do
     t.string "category"
     t.text "description"
     t.string "address"
-    t.string "album"
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -104,9 +101,9 @@ ActiveRecord::Schema[7.0].define(version: 3022_11_28_163829) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "budgets", "events"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "budgets", "events"
   add_foreign_key "invitations", "events"
   add_foreign_key "invitations", "users"
   add_foreign_key "spendings", "events"
