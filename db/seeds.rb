3619066c81981d0ef060f128a9fc256aef9f367d
Julien Nakad <julien.nakad@gmail.com>
2022-12-06 11:19:59 +0100

    modifs profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,47 +5,57 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require "faker"
+
 Invitation.destroy_all
-User.destroy_all
 Spending.destroy_all
 Budget.destroy_all
 Event.destroy_all
+User.destroy_all
 
-user_first = User.new(first_name: "Joy",
+joy = User.new(first_name: "Joy",
                       last_name: "Klasen",
                       password: "azerty",
-                      email: "jlksn@gmail.com",
-                      phone_number: "0678273867",
-                      address: "21 rue Haxo",
+                      email: "joy.klasen@gmail.com",
+                      phone_number: "603992195",
+                      address: "83 rue Abbé de l'épée 13005 Marseille",
                       admin: true)
-user_first.save!
+joy.save!
 
-user_second = User.new(first_name: "Julien",
-  last_name: "Naked",
+adrien = User.new(first_name: "Adrien",
+  last_name: "Mery-Costa",
   password: "azerty",
-  email: "jul@gmail.com",
-  phone_number: "0678273867",
-  address: "21 rue Haxo",
+  email: "psnw.adrien@gmail.com",
+  phone_number: "678495182",
+  address: "116 avenue Jules Cantini 13008 Marseille",
   admin: true)
-user_second.save!
+adrien.save!
+
+julien = User.new(first_name: "Julien",
+  last_name: "Nakad",
+  password: "azerty",
+  email: "julien.nakad@gmail.com",
+  phone_number: "635297939",
+  address: "8 rue Alexandre Pieyre 30900 Nîmes",
+  admin: true)
+julien.save!
 
 event_first = Event.new(start_date: DateTime.new(2022,2,3,4,5,6),
                         end_date: DateTime.new(2023,2,3,4,5,6),
                         category: "Wedding",
                         description: "super mariage de ouf de Joy",
                         address: "Place Castellane",
-                        title: "Mariage de Joy KLSN")
+                        title: "Mariage de Joy KLSN",
+                        user_id: julien.id)
 event_first.save!
 
-invitation = Invitation.new(user: user_first,
+invitation = Invitation.new(user: joy,
                             event: event_first,
                             status: true,
                             partner: true,
                             comment: "J'ai trop hâte d'être invité à ton mariage Joy xD ^^")
 invitation.save!
 
-invitation_second = Invitation.new(user: user_second,
+invitation_second = Invitation.new(user: julien,
   event: event_first,
   status: true,
   partner: true,
